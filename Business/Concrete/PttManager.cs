1f4bdd80a54ebb5294709bbf2cb54a57e01b1958
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-11-05 02:08:06 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+    {
+        IApplicantService _applicantService;
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {            
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi");
+            }
+            
+        }
+    }
+}
